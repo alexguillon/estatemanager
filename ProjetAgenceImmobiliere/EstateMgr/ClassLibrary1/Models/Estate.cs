5bc8daad09ea4a0940465b570c6a7e13c5580677
alexguillon <cyclopedeveloppeur@gmail.com>
2018-03-06 08:52:08 +0100

    ajout du projet initial

---
+++ ProjetAgenceImmobiliere/EstateMgr/ClassLibrary1/Models/Estate.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EstateMgrCore.Models
+{
+    public class Estate : ViewModels.BaseNotifyPropertyChanged
+    {
+      
+    }
+}
---
+++ ProjetAgenceImmobiliere/EstateMgr/ClassLibrary1/ViewModels/BaseNotifyPropertyChanged.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace EstateMgrCore.ViewModels
+{
+    public abstract class BaseNotifyPropertyChanged : INotifyPropertyChanged
+    {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+        private Dictionary<string, object> _propertyValues = new Dictionary<string, object>();
+        public T GetProperty<T>([CallerMemberName] string propertyName = null)
+        {
+            return GetProperty<T>(propertyName, out bool exists);
+        }
+        private T GetProperty<T>(string propertyName, out bool exists)
+        {
+            exists = false;
+            if (_propertyValues.ContainsKey(propertyName))
+            {
+                exists = true;
+                return (T)_propertyValues[propertyName];
+            }
+            return default(T);
+        }
+        public bool SetProperty<T>(T value, [CallerMemberName] string propertyName = null)
+        {
+            var current = GetProperty<T>(propertyName, out bool exists);
+            if (!EqualityComparer<T>.Default.Equals(current, value))
+            {
+                if (exists)
+                    _propertyValues[propertyName] = value;
+                else
+                    _propertyValues.Add(propertyName, value);
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+                return true;
+            }
+            return false;
+        }
+
+
+
+    }
+}
